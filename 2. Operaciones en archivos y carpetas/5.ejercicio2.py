426f19c2006dfc0ac082afe01152b1f5fe567de7
Carlos Giles <92232878+CarlosGiles@users.noreply.github.com>
2022-08-01 11:54:29 -0500

    Add files via upload
    add exc 5

---
+++ 2. Operaciones en archivos y carpetas/5.ejercicio2.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+# TODO: Cambiar extension de los archivos dentro de la carpeta "cambiar-extension". Primero txt --> csv, luego csv --> txt
+folder = Path('cambiar-extension')
+
+# 1. txt -> csv
+
+
+# 2. csv -> txt
